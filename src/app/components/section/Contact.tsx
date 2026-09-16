3fbf09757c083c1cf33f359dc3adce39487a6569
Abe Ahn <48481888+abrahamahn@users.noreply.github.com>
2026-09-16 12:51:39 +0900

    Remove public direct-contact details while preserving the contact form

--- src/app/components/section/Contact.tsx
+++ src/app/components/section/Contact.tsx
@@ -24,46 +24,6 @@ const Contact: React.FC<ContactProps> = ({
         <div className="title">
           <h3>Contact</h3>
         </div>
-        <div className="extra_info">
-          <ul className="wrapper">
-            <li data-aos="fade-right" data-aos-duration="1200">
-              <div className="list_inner">
-                <div className="icon">
-                  <img src="assets/contact/phone.png" alt="icon" />
-                </div>
-                <div className="content">
-                  <h3>Phone</h3>
-                  <ul>
-                    <li>
-                      <a href="Tel: +1 650 254 6342">+1 650 254 6342</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-            <li
-              data-aos="fade-right"
-              data-aos-duration="1200"
-              data-aos-delay="100"
-            >
-              <div className="list_inner">
-                <div className="icon">
-                  <img src="assets/contact/email.png" alt="icon" />
-                </div>
-                <div className="content">
-                  <h3>Email</h3>
-                  <ul>
-                    <li>
-                      <a href="mailto:satmorningrain@gmail.com">
-                        satmorningrain@gmail.com
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
         <div className="mainpart">
           <div
             className="left"
